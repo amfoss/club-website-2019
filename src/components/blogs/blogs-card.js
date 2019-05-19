715d34fbba57d35c0f1a400a5dabc72eb9f53115
harshithpabbati <pabbatiharshith@gmail.com>
2019-05-20 03:34:52 +0530

    Added the blog cards and cover image.

---
+++ src/components/blogs/blogs-card.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types"
+import React from "react"
+import { Link } from "gatsby"
+
+import avatar from "../../images/defaults/avatar.png"
+
+const BlogCard = ({ blog }) => (
+  <Link to={"/@" + blog.author + "/" + blog.slug} className="blog-card">
+    <img
+      src={blog.coverpic ? blog.coverpic : avatar}
+      alt={blog.slug + `'s image`}
+    />
+    <h3>
+      {blog.title}
+    </h3>
+  </Link>
+)
+
+BlogCard.propTypes = {
+  blog: PropTypes.any,
+}
+
+BlogCard.defaultProps = {
+  blog: null,
+}
+
+export default BlogCard
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,19 +4,25 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import BlogCard from "../components/blogs/blogs-card.js"
+import { Box } from "@rebass/grid"
+
 const Blog = ({
   data: {
     allMarkdownRemark: { edges },
   },
 }) => {
-  const Posts = edges
-    .map(edge => <div key={edge.node.id}>{edge.node.frontmatter.title}</div>)
+  const Blogs = edges.map(edge => (
+    <Box key={edge.node.id} m={4} width={[1 / 3, 1 / 4, 1 / 5]}>
+      <BlogCard blog={edge.node.frontmatter} />
+    </Box>
+  ))
 
   return (
     <Layout>
       <SEO title="Blog" />
       <h1>Blog</h1>
-      <div>{Posts}</div>
+      <div>{Blogs}</div>
     </Layout>
   )
 }
@@ -33,7 +39,9 @@ export const pageQuery = graphql`
             title
             author
             tags
+            slug
             date(formatString: "DD MMMM, YYYY")
+            coverpic
           }
         }
       }
