10b4ccc214a63b9149983dc65c90675570b46825
harshithpabbati <pabbatiharshith@gmail.com>
2019-05-25 20:33:51 +0530

    Implemented Search Bar in Members Page

--- src/pages/members.js
+++ src/pages/members.js
@@ -7,27 +7,48 @@ import SEO from "../components/seo"
 import MemberCard from "../components/members/member-card.js"
 import TitleBar from "../components/titleBar"
 
-const Members = ({
-  data: {
-    allMembersYaml: { edges },
-  },
-}) => {
-  const Members = edges.map(edge => (
-    <div key={edge.node.id} className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
-      <MemberCard member={edge.node} />
-    </div>
-  ))
 
-  return (
-    <Layout>
-      <SEO title="Members" />
-      <TitleBar title="Members" />
-      <div className="row m-0 p-4">{Members}</div>
-    </Layout>
-  )
+export default class Members extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      term: '',
+    }
+  }
+
+  handleSearch(event){
+    this.setState({
+      term: event.target.value
+    })
+  }
+
+  render() {
+    let filteredMembers = this.props.data.allMembersYaml.edges.filter(
+      (edge) =>{
+        return edge.node.username.indexOf(this.state.term) !== -1;
+      }
+    )
+    return (
+      <Layout>
+        <SEO title="Members"/>
+        <TitleBar title="Members"/>
+        <div className='bg-white row m-0' id='filter-bar'>
+          <div className='col-md-4 col-10 align-items-center justify-content-center' id='search-container'>
+            <input id="search-box" type="text" className="form-control"
+                   placeholder="Search Here"  onChange={this.handleSearch.bind(this)} />
+          </div>
+        </div>
+        <div className="row m-0 p-4">
+          {filteredMembers.map(edge => (
+            <div key={edge.node.id} className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+              <MemberCard member={edge.node} />
+            </div>
+          ))}
+        </div>
+      </Layout>
+    )
+  }
 }
-
-export default Members
 
 export const pageQuery = graphql`
   query {
