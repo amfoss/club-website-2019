8f5aafe27a0d1676b7225a479d8061a26441ae61
ashwin <aswinshenoy65@gmail.com>
2019-05-27 12:35:02 +0530

    Add Support for GSoC Year Filtering

--- src/components/achievements/gsoc.js
+++ src/components/achievements/gsoc.js
@@ -1,6 +1,51 @@
-import React from "react"
+import React, { useState }  from "react"
 import { StaticQuery, graphql } from "gatsby"
-import MemberList from "./member-list"
+import getProfiles from "../theme/getProfiles"
+import ListCard from "../theme/list-card"
+
+const MemberList = ({ members }) => {
+  let years = [...new Set( members.map(obj => obj.node.Year)) ]
+  years =  Array.from(years[0])
+  const [year, setYear] = useState(2019);
+  const profiles = getProfiles()
+  let filtered = members.filter( member => member.node.Year == year)
+  return (
+    <div className="list-card card">
+      <div className="list-heading d-flex">
+        <div className="w-75">Google Summer of Code</div>
+        <div className="w-25">
+          <select
+              name="years"
+              onChange={event => setYear(event.target.value)}
+              value={year}
+          >
+            {
+              years.map((year, key) =>
+                 <option key={key} value={year}>{year}</option>
+              )
+            }
+          </select>
+        </div>
+      </div>
+      {
+        filtered.map(member => {
+          let profile = profiles.find(
+            profile => profile.node.username === member.node.Member
+          )
+          return profile ?
+                <ListCard
+                  key={member.node.id}
+                  username={profile.node.username}
+                  firstName={profile.node.firstName}
+                  lastName={profile.node.lastName}
+                  tagline={member.node.Org}
+                  avatar={profile.node.avatar ? profile.node.avatar.childImageSharp.resize.src : null}
+                /> : null
+        })
+      }
+    </div>
+  )
+}
 
 export default () => (
   <StaticQuery
@@ -21,8 +66,7 @@ export default () => (
     `}
     render={data => (
       <div className="p-2 my-4">
-        <h5 className="ml-2 mb-4">Google Summer of Code 2019</h5>
-        <MemberList members={data.allGSoCYaml.edges} />
+          <MemberList members={data.allGSoCYaml.edges} />
       </div>
     )}
   />
