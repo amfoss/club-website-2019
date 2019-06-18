8d315f6300c85d55cb9da35983200aae3902c746
harshithpabbati <pabbatiharshith@gmail.com>
2019-06-18 22:50:18 +0530

    Fix Social Icons

--- src/components/theme/social-icon.js
+++ src/components/theme/social-icon.js
@@ -10,15 +10,15 @@ import website from "../../images/icons/website.png"
 import chatroom from "../../images/icons/chatroom.png"
 
 const SocialIcon = ({ name, link }) => (
-  <a href={link} className="social-icon">
-    {name === "facebook" ? <img src={facebook} alt={name} /> : null}
-    {name === "twitter" ? <img src={twitter} alt={name} /> : null}
-    {name === "instagram" ? <img src={instagram} alt={name} /> : null}
-    {name === "linkedin" ? <img src={linkedin} alt={name} /> : null}
-    {name === "github" ? <img src={github} alt={name} /> : null}
-    {name === "website" ? <img src={website} alt={name} /> : null }
-    {name === "chatroom" ? <img src={chatroom} alt={name} /> : null}
-  </a>
+  link ? <a href={link} className="social-icon">
+    {name === "facebook" ? <img src={facebook} alt={name}/> : null}
+    {name === "twitter" ? <img src={twitter} alt={name}/> : null}
+    {name === "instagram" ? <img src={instagram} alt={name}/> : null}
+    {name === "linkedin" ? <img src={linkedin} alt={name}/> : null}
+    {name === "github" ? <img src={github} alt={name}/> : null}
+    {name === "website" ? <img src={website} alt={name}/> : null}
+    {name === "chatroom" ? <img src={chatroom} alt={name}/> : null}
+  </a> : null
 )
 
 SocialIcon.propTypes = {
@@ -36,8 +36,7 @@ SocialIcon.propTypes = {
 }
 
 SocialIcon.defaultProps = {
-  name: `web`,
-  link: `#`,
+  name: `web`
 }
 
 export default SocialIcon
