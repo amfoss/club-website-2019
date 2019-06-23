5c9bc830146cefdf1353260a05eb0ed6669717bc
ashwin <aswinshenoy65@gmail.com>
2019-06-23 14:03:51 +0530

    Remove Profiles for Hidden Members, Update Order of Alumni Companies

--- src/templates/profileTemplate.js
+++ src/templates/profileTemplate.js
@@ -12,34 +12,40 @@ export default ({ data: { membersYaml } }) => {
       <SEO title={member.firstName + " " + member.lastName} />
       <section id="cover">
         <div className="container d-md-flex justify-content-center">
-          <div className="d-flex align-items-center justify-content-center">
-            <img
-              src={
-                member.avatar
-                  ? member.avatar.childImageSharp.fluid.src
-                  : avatar
-              }
-              alt={member.firstName + " " + member.lastName + `'s photo`}
-              id="profile-pic"
-              className="rounded-circle"
-            />
-          </div>
-          <div className="ml-4">
-            <div className="name text-left">
-              <div className={"d-inline-block text-left role-tag my-4 " + member.role}>{member.role}</div>
-              <h1>{member.firstName} {member.lastName}</h1>
-              <h5 className="tagline">{member.tagline}</h5>
-            </div>
-            {member.links ? (
-              <div className="social-links">
-                <SocialIcon name="github" link={member.links.github} />
-                <SocialIcon name="facebook" link={member.links.facebook} />
-                <SocialIcon name="twitter" link={member.links.twitter} />
-                <SocialIcon name="instagram" link={member.links.instagram} />
-                <SocialIcon name="linkedin" link={member.links.linkedin} />
-              </div>
-            ) : null}
-          </div>
+          {!member.hide ? (
+              <>
+                <div className="d-flex align-items-center justify-content-center">
+                  <img
+                    src={
+                      member.avatar
+                        ? member.avatar.childImageSharp.fluid.src
+                        : avatar
+                    }
+                    alt={member.firstName + " " + member.lastName + `'s photo`}
+                    id="profile-pic"
+                    className="rounded-circle"
+                  />
+                </div>
+                <div className="ml-4">
+                  <div className="name text-left">
+                    <div className={"d-inline-block text-left role-tag my-4 " + member.role}>{member.role}</div>
+                    <h1>{member.firstName} {member.lastName}</h1>
+                    <h5 className="tagline">{member.tagline}</h5>
+                  </div>
+                  {member.links ? (
+                    <div className="social-links">
+                      <SocialIcon name="github" link={member.links.github} />
+                      <SocialIcon name="facebook" link={member.links.facebook} />
+                      <SocialIcon name="twitter" link={member.links.twitter} />
+                      <SocialIcon name="instagram" link={member.links.instagram} />
+                      <SocialIcon name="linkedin" link={member.links.linkedin} />
+                    </div>
+                  ) : null}
+                </div>
+              </>
+            ):(
+             <h1>No profile exists for the member you are looking. :( </h1>
+          )}
         </div>
       </section>
     </Layout>
@@ -53,6 +59,7 @@ export const pageQuery = graphql`
       lastName
       username
       role
+      hide
       tagline
       links {
         github
