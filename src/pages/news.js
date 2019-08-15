ee53ae4faa0f326f80900f88542ddb3bafea5eff
lohitha <lohithabhagam123@gmail.com>
2019-08-15 22:45:29 +0530

    Add Search Feature for News page

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -11,7 +11,12 @@ const News = ({
     allMarkdownRemark: { edges },
   },
 }) => {
-  const Articles = edges.map(edge => (
+  const [query, setQuery] = useState('');
+  const filter =  edges.filter(edge => {
+    if(edge.node.frontmatter.title.toLowerCase().startsWith(query.toLowerCase()))
+      return 1
+  })
+  const Articles = filter.map(edge => (
     <div key={edge.node.id} className="p-2">
       <NewsCard article={edge.node.frontmatter} />
     </div>
@@ -21,9 +26,25 @@ const News = ({
     <Layout>
       <SEO title="News" />
       <TitleBar title="News" />
-      <div className="row m-0">
-        <div className="col-md-8 col-lg-9 p-2">
-          {Articles}
+      <div className="row m-0 p-1">
+        <div className="col-md-8 col-lg-9 p-2 order-2 order-md-1">
+          <div className="row mx-2 my-4">{Articles}</div>
+        </div>
+        <div className="col-md-4 col-lg-3 order-md-2 order-1 px-2 py-4">
+          <div className="card p-4 position-sticky" style={{ top: "1rem" }} id="filter-card">
+            <h5 className="my-3">Search & Filter</h5>
+            <div className="mx-2">
+              <div>Search by Name</div>
+              <input
+                id="search-box"
+                type="text"
+                className="form w-100 p-2 mt-2"
+                placeholder="Search Here"
+                onChange={e => setQuery(e.target.value)}
+              />
+              <hr />
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
