4209f836d7735be9acbfb304cccc484d83cfd525
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-02-03 02:17:07 +0530

    pages/events: Update the formatting This commit does the following - makes indentation consistent

--- src/pages/events/hacktoberfest.js
+++ src/pages/events/hacktoberfest.js
@@ -1,8 +1,6 @@
 import React from "react"
 import HacktoberFest from "../../components/events/hacktoberfest"
 
-const Events = () => (
-  < HacktoberFest/>
-)
+const Events = () => <HacktoberFest />
 
 export default Events
