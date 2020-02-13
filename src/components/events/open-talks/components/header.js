4839d30fa19914499ace3ce08bc3159e869e54cc
harshithpabbati <pabbatiharshith@gmail.com>
2020-02-13 17:48:52 +0530

    header: Remove canvas animations

--- src/components/events/open-talks/components/header.js
+++ src/components/events/open-talks/components/header.js
@@ -1,111 +1,9 @@
 import React, { useEffect } from "react"
 import amFOSSLogo from "../../../../../src/images/amfoss_logo.png"
 import amritaLogo from "../../../../../src/images/amrita_logo.png"
+import HeaderImage from "../images/header.png"
 
 const Header = () => {
-  useEffect(() => {
-    let canvas = document.querySelector("#opentalks"),
-      ctx = canvas.getContext("2d"),
-      particles = [],
-      amount = 0,
-      mouse = { x: 0, y: 0 },
-      radius = 1.0
-
-    let colors = ["#FFFF00"]
-
-    let copy = "Open Talks"
-
-    let ww = (canvas.width = window.innerWidth)
-    let wh = (canvas.height = window.innerHeight)
-
-    function Particle(x, y) {
-      this.x = Math.random() * ww
-      this.y = Math.random() * wh
-      this.dest = {
-        x: x,
-        y: y,
-      }
-      this.r = Math.random() + 1
-      this.vx = 20
-      this.vy = 20
-      this.accX = 0.05
-      this.accY = 0.05
-      this.friction = Math.random() * 0.05 + 0.88
-
-      this.color = colors[0]
-    }
-
-    Particle.prototype.render = function() {
-      this.accX = (this.dest.x - this.x) / 300
-      this.accY = (this.dest.y - this.y) / 300
-      this.vx += this.accX
-      this.vy += this.accY
-      this.vx *= this.friction
-      this.vy *= this.friction
-
-      this.x += this.vx
-      this.y += this.vy
-
-      ctx.fillStyle = this.color
-      ctx.beginPath()
-      ctx.arc(this.x, this.y, this.r, Math.PI * 2, false)
-      ctx.fill()
-
-      let a = this.x - mouse.x
-      let b = this.y - mouse.y
-
-      let distance = Math.sqrt(a * a + b * b)
-      if (distance < radius * 70) {
-        this.accX = (this.x - mouse.x) / 100
-        this.accY = (this.y - mouse.y) / 100
-        this.vx += this.accX
-        this.vy += this.accY
-      }
-    }
-
-    function onTouchEnd(e) {
-      mouse.x = -9999
-      mouse.y = -9999
-    }
-
-    function initScene() {
-      ww = canvas.width = window.innerWidth
-      wh = canvas.height = window.innerHeight
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-      ctx.font = "bold " + ww / 10 + "px sans-serif"
-      ctx.textAlign = "center"
-      ctx.fillText(copy, ww / 2, wh / 2.7)
-
-      let data = ctx.getImageData(0, 0, ww, wh).data
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      ctx.globalCompositeOperation = "screen"
-
-      particles = []
-      for (let i = 0; i < ww; i += Math.round(ww / 250)) {
-        for (let j = 0; j < wh; j += Math.round(ww / 250)) {
-          if (data[(i + j * ww) * 4 + 3] > 0) {
-            particles.push(new Particle(i, j))
-          }
-        }
-      }
-      amount = particles.length
-    }
-
-    function render(a) {
-      requestAnimationFrame(render)
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      for (let i = 0; i < amount; i++) {
-        particles[i].render()
-      }
-    }
-
-    window.addEventListener("resize", initScene)
-    window.addEventListener("touchend", onTouchEnd)
-    initScene()
-    requestAnimationFrame(render)
-  })
   return (
     <>
       <div id="header-area">
@@ -122,11 +20,12 @@ const Header = () => {
           </div>
         </div>
         <div className="header-title">
-          <div className="row m-0 w-100">
-            <div id="particle-container">
-              <canvas id="opentalks" />
+          <div className="row m-0 w-100 p-4 px-5">
+            <div className="col-sm-2" />
+            <div className="col-sm-8">
+              <img src={HeaderImage} style={{width: "100%"}} />
             </div>
-            <div className="col-sm-12">
+            <div className="col-sm-12 py-5">
               <div className="d-flex align-items-center text-lg-left text-center justify-content-center h-100">
                 <div>
                   <h3 className="d-flex align-items-center text-center justify-content-center h-100">
