c92bd3e96460d4950cf926651e2f5088c214d624
Akhil K Gangadharan <helloimakhil@gmail.com>
2020-02-16 18:19:16 +0530

    content: Rewrite and rephrase most of the content
    components:home: Update years

--- src/components/home/homeLanding.js
+++ src/components/home/homeLanding.js
@@ -36,7 +36,7 @@ export default () => {
             </h1>
             <p className="d-none d-md-block">
               amFOSS, a student-run community with over 50+ members from Amrita
-              Vishwa Vidyapeetham, Amritapuri, over the last 12 years, has
+              Vishwa Vidyapeetham, Amritapuri, over the last 14 years, has
               helped 100+ passionate students to transform into successful &
               innovative engineers who today work across the world in Fortune
               500 Companies loving what they do.
