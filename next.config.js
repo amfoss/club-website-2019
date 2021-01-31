6f0db38555a944fc44f3e00e6f1dedd8df30d9a0
Harshith Pabbati <pabbatiharshith@gmail.com>
2021-01-31 14:34:16 +0000

    Add nextsource maps

--- next.config.js
+++ next.config.js
@@ -1,35 +1,31 @@
-module.exports = {};
-
+const withPlugins = require('next-compose-plugins');
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
+const withSourceMaps = require('@zeit/next-source-maps');
 
-const withYAML = require('next-yaml');
-module.exports = withYAML(module.exports);
-module.exports = {
-  siteMetadata: {
-    title: `amFOSS (FOSS@Amrita)`,
-    description: `A student community based in Amrita Vishwa Vidyapeetham, Amritapuri focused on contributing to FOSS and mentoring students to achieve excellence.`,
-    author: 'amfoss',
-    siteUrl: 'https://amfoss.in/',
+const customConfig = {
+  target: 'serverless',
+  devIndicators: {
+    autoPrerender: false,
   },
   trailingSlash: true,
-};
-module.exports = withCSS(
-  withSass({
-    webpack(config, options) {
-      target: 'serverless',
-        config.module.rules.push({ test: /\.md$/, use: 'raw-loader' });
-      config.module.rules.push({ test: /\.yml$/, use: 'raw-loader' });
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-          },
+  webpack(config, options) {
+    config.module.rules.push({ test: /\.md$/, use: 'raw-loader' });
+    config.module.rules.push({ test: /\.yml$/, use: 'raw-loader' });
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
         },
-      });
-      return config;
-    },
-  })
+      },
+    });
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [[withSourceMaps], [withCSS], [withSass]],
+  customConfig
 );
