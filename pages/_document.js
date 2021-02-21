3c0820a160d37faaa51f7361e518a06d91c71d8d
harshithpabbati <pabbatiharshith@gmail.com>
2021-02-21 12:44:01 +0530

    Add Google Analytics

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            rel="preconnect"
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-115377745-1`}
+          />
+          <script
+              nonce="{GENERATED_NONCE}"
+              dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-115377745-1');`,
+              }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
