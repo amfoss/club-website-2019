0e9d2f9ed8d39a6a6c4d8fe7dfa254d289a8e490
Akhil K Gangadharan <akhilam512@gmail.com>
2021-08-22 22:56:19 +0530

    alumniSuccess: add new logos and university logo section

--- components/home/alumniSuccess.js
+++ components/home/alumniSuccess.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import json from '../../content/alumniImage.json';
+import companyJson from '../../content/alumniCompanyImage.json';
+import universityJson from '../../content/alumniUniversityImage.json';
+
 export default () => {
   return (
     <div className="text-center bg-white" style={{ padding: '4vh 0' }}>
-      <h3 className="mb-4"> Our Alumni Work at</h3>
+      <h3 className="mb-4"> Our Alumni are at </h3>
       <div style={{ padding: '0 5vw' }}>
-        {json.map((edge) => (
+        {companyJson.map((edge) => (
+          <img
+            src={edge.image}
+            style={{
+              height: '2vmax',
+              maxHeight: '40px',
+              minHeight: '28px',
+              margin: '1vmax',
+            }}
+          />
+        ))}
+      </div>
+      <hr style={{ textAlign: 'center', width: '80%' }} />
+      <div style={{ padding: '0 5vw' }}>
+        {universityJson.map((edge) => (
           <img
             src={edge.image}
             style={{
