2413774be905a021c270a2681049a27bc74c8a4e
govindgoel <govindgoel82@gmail.com>
2021-09-22 18:47:26 +0530

    pages: add training page

---
+++ pages/arduinoL1.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const Arduino = () => {
+  useEffect(() => {
+    window.location.href = 'https://training.amfoss.in/course/arduinoL1.html';
+  }, []);
+
+  return <p></p>;
+};
+
+export default Arduino;
