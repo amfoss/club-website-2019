622f09c66a5ad48668ec9bdca43eec615d2cc7e4
Ash-004 <ash_004@icloud.com>
2023-06-01 08:30:27 +0530

    made date display dynamic

--- pages/members/index.js
+++ pages/members/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
 
@@ -26,7 +25,22 @@ const query = ` query {
       }
   }`;
 
+
+//uncomment when fetchdata is in use
+// let years = []
+// const fetchData = async () => await dataFetch({ query });
+// fetchData().then(result => {
+//     const activeUsers = result.data.activeUsers;
+//     const batches = activeUsers.map(user => user.profile.batch);
+//     const currentYear = Math.max(...batches);
+//     for (let year = currentYear; year >= 2017; year--) {
+//         years.push(year);
+//         console.log(years)
+//     }
+// })
+
 export default class Index extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -37,7 +51,6 @@ export default class Index extends React.Component {
       loaded: false,
     };
   }
-
   // fetchData = async () => await dataFetch({ query });
 
   goTop = () => {
@@ -71,7 +84,31 @@ export default class Index extends React.Component {
     });
   }
 
+  // componentDidMount() {
+  //
+  //   const fetchData = async () => await dataFetch({ query });
+  //
+  //   fetchData().then(result => {
+  //     this.setState({
+  //       data: result.data.activeUsers,
+  //       loaded: true,
+  //     });
+  //   })
+  // }
+
+
   render() {
+
+// For use when datafetch not in use.
+
+      const currentYear = new Date().getFullYear();
+      const years = []
+    for (let year = currentYear; year >= 2017; year--) {
+      years.push(year);
+    }
+
+
+
     let filteredMembers = this.state.data.filter((user) => {
       let qflag = 1;
       let yflag = 1;
@@ -84,19 +121,19 @@ export default class Index extends React.Component {
       // Search matches firstname and lastname
       if (user.firstName) {
         user.firstName
-          .toLowerCase()
-          .split(' ')
-          .forEach((part) => {
-            if (part.startsWith(query)) qflag = 1;
-          });
+            .toLowerCase()
+            .split(' ')
+            .forEach((part) => {
+              if (part.startsWith(query)) qflag = 1;
+            });
       }
       if (user.lastName) {
         user.lastName
-          .toLowerCase()
-          .split(' ')
-          .forEach((part) => {
-            if (part.startsWith(query)) qflag = 1;
-          });
+            .toLowerCase()
+            .split(' ')
+            .forEach((part) => {
+              if (part.startsWith(query)) qflag = 1;
+            });
       }
       if (this.state.filterYear !== 'everyone') {
         yflag = 0;
@@ -110,116 +147,114 @@ export default class Index extends React.Component {
     });
 
     return (
-      <Layout>
-        <SEO title="Members" description="Meet the official amFOSS team members" />
-        <TitleBar title="Members" id="members" />
-        <a
-          style={{
-            position: 'fixed',
-            right: '1rem',
-            bottom: '1rem',
-            backgroundColor: '#ffc107',
-            borderRadius: '100vh',
-            fontSize: '1.2rem',
-            zIndex: 5000,
-          }}
-          onClick={() => {
-            this.goTop();
-          }}
-          className="fas fa-angle-up p-3"
-        />
-        <div className="mx-4 mt-4">
-          <p>
-            Showing {filteredMembers.length} out of {this.state.data.length} members
-          </p>
-        </div>
-        <div className="row m-0">
-          <div className="col-md-8 col-lg-9 order-2 order-md-1">
-            <div className="row m-0 mb-4">
-              {this.state.loaded ? (
-                filteredMembers
-                  .sort((a, b) => a.firstName.localeCompare(b.firstName))
-                  .map((user) =>
-                    user.profile.batch && user.profile.displayInWebsite ? (
-                      <div
-                        key={user.username}
-                        className="col-6 col-md-6 col-lg-4 col-xl-3 p-2"
-                      >
-                        <MemberCard
-                          username={user.username}
-                          firstName={user.firstName}
-                          lastName={user.lastName}
-                          tag={user.profile.role}
-                          profilePic={user.profile.profilePic}
-                          githubUsername={user.profile.githubUsername}
-                        />
-                      </div>
-                    ) : null
-                  )
-              ) : (
-                <ReactLoading type="spinningBubbles" color="#000" />
-              )}
-            </div>
+        <Layout>
+          <SEO title="Members" description="Meet the official amFOSS team members" />
+          <TitleBar title="Members" id="members" />
+          <a
+              style={{
+                position: 'fixed',
+                right: '1rem',
+                bottom: '1rem',
+                backgroundColor: '#ffc107',
+                borderRadius: '100vh',
+                fontSize: '1.2rem',
+                zIndex: 5000,
+              }}
+              onClick={() => {
+                this.goTop();
+              }}
+              className="fas fa-angle-up p-3"
+          />
+          <div className="mx-4 mt-4">
+            <p>
+              Showing {filteredMembers.length} out of {this.state.data.length} members
+            </p>
           </div>
-          <div className="col-md-4 col-lg-3 order-md-2 order-1 px-2 py-3">
-            <div
-              className="card p-4 position-sticky"
-              style={{ top: '1rem' }}
-              id="filter-card"
-            >
-              <h5 className="my-3">Search & Filter</h5>
-              <div className="mx-2">
-                <div>Search by Name</div>
-                <input
-                  id="search-box"
-                  type="text"
-                  className="form w-100 p-2 mt-2"
-                  placeholder="Search Here"
-                  onChange={this.handleSearch.bind(this)}
-                />
-                <hr />
-              </div>
-              <div className="mb-4 mx-2">
-                <div>Filter By Year</div>
-                <select
-                  className="bg-white p-2 w-100 mt-2"
-                  onChange={this.memberFilter.bind(this)}
-                  value={this.state.filterYear}
-                >
-                  <option value="everyone">
-                    {this.state.filterYear === 'Everyone'
-                      ? 'Change Year'
-                      : ' Everyone'}
-                  </option>
-                  <option value="2021">2021</option>
-                  <option value="2020">2020</option>
-                  <option value="2019">2019</option>
-                  <option value="2018">2018</option>
-                  <option value="2017">2017</option>
-                </select>
-              </div>
-              <div className="mb-4 mx-2">
-                <div>Filter By Role</div>
-                <select
-                  className="bg-white p-2 w-100 mt-2"
-                  onChange={this.memberRoleFilter.bind(this)}
-                  value={this.state.filterRole}
-                >
-                  <option value="everyone">
-                    {this.state.filterRole === 'Everyone'
-                      ? 'Change Role'
-                      : ' Everyone'}
-                  </option>
-                  <option value="Member">Member</option>
-                  <option value="Mentor">Mentor</option>
-                  <option value="Alumni">Alumni</option>
-                </select>
+          <div className="row m-0">
+            <div className="col-md-8 col-lg-9 order-2 order-md-1">
+              <div className="row m-0 mb-4">
+                {this.state.loaded ? (
+                    filteredMembers
+                        .sort((a, b) => a.firstName.localeCompare(b.firstName))
+                        .map((user) =>
+                            user.profile.batch && user.profile.displayInWebsite ? (
+                                <div
+                                    key={user.username}
+                                    className="col-6 col-md-6 col-lg-4 col-xl-3 p-2"
+                                >
+                                  <MemberCard
+                                      username={user.username}
+                                      firstName={user.firstName}
+                                      lastName={user.lastName}
+                                      tag={user.profile.role}
+                                          // profilePic={user.profile.profilePic}
+                                      githubUsername={user.profile.githubUsername}
+                                  />
+                                </div>
+                            ) : null
+                        )
+                ) : (
+                    <ReactLoading type="spinningBubbles" color="#000" />
+                )}
               </div>
             </div>
+            <div className="col-md-4 col-lg-3 order-md-2 order-1 px-2 py-3">
+              <div
+                  className="card p-4 position-sticky"
+                  style={{ top: '1rem' }}
+                  id="filter-card"
+              >
+                <h5 className="my-3">Search & Filter</h5>
+                <div className="mx-2">
+                  <div>Search by Name</div>
+                  <input
+                      id="search-box"
+                      type="text"
+                      className="form w-100 p-2 mt-2"
+                      placeholder="Search Here"
+                      onChange={this.handleSearch.bind(this)}
+                  />
+                  <hr />
+                </div>
+                <div className="mb-4 mx-2">
+                  <div>Filter By Year</div>
+                  <select
+                      className="bg-white p-2 w-100 mt-2"
+                      onChange={this.memberFilter.bind(this)}
+                      value={this.state.filterYear}
+                  >
+                    <option value="everyone">
+                      {this.state.filterYear === 'Everyone'
+                          ? 'Change Year'
+                          : ' Everyone'}
+                    </option>
+                    {years.map((year) => (
+                        <option value={year}>{year}</option>
+                    ))}
+                  </select>
+                </div>
+                <div className="mb-4 mx-2">
+                  <div>Filter By Role</div>
+                  <select
+                      className="bg-white p-2 w-100 mt-2"
+                      onChange={this.memberRoleFilter.bind(this)}
+                      value={this.state.filterRole}
+                  >
+                    <option value="everyone">
+                      {this.state.filterRole === 'everyone'
+                          ? 'Change Role'
+                          : ' Everyone'}
+                    </option>
+                    <option value="Member">Member</option>
+                    <option value="Mentor">Mentor</option>
+                    <option value="Alumni">Alumni</option>
+                  </select>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <CookieConsent />
-      </Layout>
+          <CookieConsent />
+        </Layout>
     );
   }
 }
