30b43c0e4b5521ad6cbb0e0d41158437e9467192
Abhinav <karriabhinav15@gmail.com>
2023-11-02 23:59:34 +0530

    Feat: connected members page to CMS

--- components/theme/memberCard.js
+++ components/theme/memberCard.js
@@ -14,36 +14,17 @@ const MemberCard = ({
   profilePic,
 }) => (
   <div className="member-card card">
-    <a href={''}>
-      {profilePic ? (
-        <Image
-          src={profilePic}
-          alt="avatar"
-          width={100}
-          height={100}
-          className="avatar"
-        />
-      ) : githubUsername ? (
-        <Image
-          src={`https://avatars.githubusercontent.com/${githubUsername}`}
-          alt="avatar"
-          width={100}
-          height={100}
-          className="avatar"
-        />
-      ) : (
-        <img
+    <a href={'/@' + username}>
+      <img
           src={
-            githubUsername
-              ? `https://avatars.githubusercontent.com/${githubUsername}`
-              : defaultAvatar
+            profilePic
+                ? `https://api.amfoss.in/${profilePic}`
+                : githubUsername
+                    ? `https://avatars.githubusercontent.com/${githubUsername}`
+                    : defaultAvatar
           }
-          alt="avatar"
-          width={100}
-          height={100}
-          className="avatar"
-        />
-      )}
+          alt={firstName + ' ' + lastName + `'s photo`}
+      />
       <div className={'role-tag ' + tag}>{tag}</div>
       <div>
         {' '}
